a03adf0d089d9ea5073aabdc3cbd2211732db51a
David-Moisan <dmoisan.23@gmail.com>
2023-03-29 21:44:00 +0200

    Feat: Add configuration on CRUD's like filters, rules and fields

--- src/Controller/Admin/ConferenceCrudController.php
+++ src/Controller/Admin/ConferenceCrudController.php
@@ -3,7 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Conference;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
 class ConferenceCrudController extends AbstractCrudController
 {
@@ -12,14 +18,27 @@ class ConferenceCrudController extends AbstractCrudController
         return Conference::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Conference')
+            ->setEntityLabelInPlural('Conferences')
+            ->setSearchFields(['city', 'year'])
+            ->setDefaultSort(['year' => 'DESC']);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(EntityFilter::new('id'));
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield TextField::new('city');
+        yield TextField::new('year');
+        yield BooleanField::new('isInternational');
+
+        yield AssociationField::new('comments');
     }
-    */
 }
