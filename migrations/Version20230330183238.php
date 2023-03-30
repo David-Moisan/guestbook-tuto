4d133379050c4e7249b8fa3776763e6538e08e60
David-Moisan <dmoisan.23@gmail.com>
2023-03-30 20:48:13 +0200

    Fix: Bug on Session DB because without Doctrine

--- migrations/Version20230330183238.php
+++ migrations/Version20230330183238.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230330172207 extends AbstractMigration
+final class Version20230330183238 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,22 +20,13 @@ final class Version20230330172207 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('
-            CREATE TABLE session (
-                sess_id VARCHAR(128) NOT NULL PRIMARY KEY,
-                sess_data BYTEA NOT NULL,
-                sess_lifetime INTEGER NOT NULL,
-                sess_time INTEGER NOT NULL
-            )
-        ');
-        $this->addSql('CREATE INDEX expiry ON session (sess_lifetime)');
+        $this->addSql('CREATE SEQUENCE sessions_sess_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE session_sess_id_seq CASCADE');
-        $this->addSql('DROP TABLE session');
+        $this->addSql('DROP SEQUENCE sessions_sess_id_seq CASCADE');
     }
 }
