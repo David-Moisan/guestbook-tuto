3941143f65b1aafbc5d6f70203d1393f92b2732c
David-Moisan <dmoisan.23@gmail.com>
2023-04-01 04:44:41 +0200

    Feat: Block Spam Comment With an API and SPamChecker

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,7 +7,9 @@ use App\Entity\Conference;
 use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
+use RuntimeException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -36,6 +38,7 @@ class ConferenceController extends AbstractController
         Conference $conference,
         CommentRepository $commentRepository,
         ConferenceRepository $conferenceRepository,
+        SpamChecker $spamChecker,
         #[Autowire('%photo_dir%')] string $photoDir
     ): Response {
         $comment = new Comment();
@@ -55,6 +58,18 @@ class ConferenceController extends AbstractController
             }
 
             $this->entityManager->persist($comment);
+
+            $context = [
+                'user_ip' => $request->getClientIp(),
+                'user_agent' => $request->headers->get('user-agent'),
+                'referrer' => $request->headers->get('referer'),
+                'permalink' => $request->getUri(),
+            ];
+
+            if (2 === $spamChecker->getSpamScore($comment, $context)) {
+                throw new RuntimeException('Blatant spam, go away !');
+            }
+
             $this->entityManager->flush();
 
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
