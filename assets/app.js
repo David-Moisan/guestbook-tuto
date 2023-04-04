a253ec16084dd923ecfdba8aadf77785cdefccf6
David-Moisan <dmoisan.23@gmail.com>
2023-04-04 16:37:04 +0200

    Front: Add Style

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,11 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
+import 'bootstrap';
+import bsCustomFileInput from 'bs-custom-file-input';
 
 // start the Stimulus application
 import './bootstrap';
+
+bsCustomFileInput.init();
